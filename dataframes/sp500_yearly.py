72c220feef8e45044b9f516248f9148511cef74b
Kirk byers <kingbyers@gmail.com>
2017-02-24 23:05:33 -0600

    Create yearly joined table function.

--- dataframes/sp500_yearly.py
+++ dataframes/sp500_yearly.py
@@ -1,22 +1,22 @@
-'''build sp500 df'''
+'''Build'''
+import datetime as dt
 import pickle
 import pandas as pd
 
-def build_sp500_df():
-    '''Builds df for sp 500'''
+def build_sp500_yearly_df(year):
+    '''Given a year build a df of closing costs'''
     with open('sp500tickers.pickle', 'rb') as file:
         tickers = pickle.load(file)
 
     main_df = pd.DataFrame()
 
     for count, ticker in enumerate(tickers):
-        #Try, some tickers are invalid
         try:
             df = pd.read_csv('data/{}.csv'.format(ticker))
             df.set_index('Date', inplace=True)
 
             df.rename(columns = {'Adj Close': ticker}, inplace=True)
-            df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace = True)
+            df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
 
             if main_df.empty:
                 main_df = df
@@ -26,8 +26,11 @@ def build_sp500_df():
             pass
 
         if count % 10 == 0:
-            print count
+            print(count)
 
-    print(main_df.head())
-    main_df.to_csv('sp500_joined_closes.csv')
-    return main_df
+    start_date = str(year) + '-01-01'
+    end_date = str(year) + '-12-31'
+    
+    print(main_df[start_date:end_date].head())
+    main_df[start_date:end_date].to_csv('sp500_joined_closes_{}.csv'.format(year))
+    return main_df[start_date:end_date]
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 '''A Test dumby doc'''
-from train.single_company import fit_ticker
-from plots.plot_sp500_corr import plot_sp500_corr
+import pandas as pd
+# from train.single_company import fit_ticker
+#from plots.plot_sp500_corr import plot_sp500_corr
+from dataframes.sp500_yearly import build_sp500_yearly_df
 
 # plot_sp500_corr()
 # extract_featursets('XOM')
 # fit_ticker('BAC')
+
+# build_sp500_yearly_df(2015)
+df = pd.read_csv('sp500_joined_closes_2015.csv', index_col=0)
+
+print(df.head())
